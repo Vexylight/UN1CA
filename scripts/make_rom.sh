3e5c50232f009c7b54fe02e6f89ce2f465732854
Vexylight <vexlight99@gmail.com>
2026-06-29 10:33:38 -0400

    Update make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -131,12 +131,6 @@ if $BUILD_ROM; then
         LOG_STEP_OUT
     fi
 
-    if [ -d "$SRC_DIR/unica/mods" ]; then
-        LOG_STEP_IN true "Applying ROM mods"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/mods" || exit 1
-        LOG_STEP_OUT
-    fi
-
     if [ -d "$APKTOOL_DIR" ]; then
         LOG_STEP_IN true "Building APKs/JARs"
         while IFS= read -r f; do
